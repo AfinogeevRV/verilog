5dd7297de5d55a56d7e85fbaba3026b938ac5306
Rustik <afinogeevrv@gmail.com>
2018-03-22 08:48:05 +0300

    pp

--- cordic/SinusCordic/work/reset_block/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity reset_block is
-    port(
-        clk             : in     vl_logic;
-        reset           : out    vl_logic
-    );
-end reset_block;
